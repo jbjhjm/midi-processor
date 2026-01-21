dcb5bad288e16d5b33bf01a6271f1651fb5edac4
jbjhjm <jannik.wjm@gmail.com>
2026-01-21 21:36:24 +0100

    migrator: JBMH Spee

---
+++ src/migrators/jbmh_speed.ts
@@ -0,0 +1,32 @@
+import color from 'ansi-colors';
+import type { MidiFile } from "midifile-ts";
+import { inRange, isMidiNote } from '../utils/matchers.js';
+
+const allowedChannels = [8].map(v=>v-1); // WARNING: 0-15 !!!
+
+
+export default async function (midi:MidiFile, file:string) {
+	let changes = [];
+	for(const event of midi.tracks[0]){
+		if(!isMidiNote(event)) continue;
+		if(!allowedChannels.includes(event.channel)) continue;
+		if(inRange(event.noteNumber, 1, 9)) {
+			// mic brightness 100%, 50% 25%, 10%
+			// --> ch 4 @ 001
+			const index = event.noteNumber - 1;
+			event.velocity = [100,80,60,40,30,20,10,5,0][index];
+			event.channel = 3-1;  // WARNING: 0-15 !!!
+			event.noteNumber = 2;
+			changes.push(event);
+		}
+	}
+
+	if(changes.length > 0) {
+		process.stdout.write(color.green('Updated '+changes.length/2+' MIDI notes in file '+file+'\n'))
+		return true;
+	} else {
+		process.stdout.write(color.gray('No changes in file '+file+'\n'))
+		return false;
+	}
+
+}
