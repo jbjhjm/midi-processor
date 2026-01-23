66399b4dfe20204b031bac4c6817d6d2cf6d0b0f
jbjhjm <jannik.wjm@gmail.com>
2026-01-23 20:04:52 +0100

    bugfixes in midi tools

--- src/utils/midi.ts
+++ src/utils/midi.ts
@@ -19,6 +19,7 @@ export function insertNoteWithLength(track:AnyEvent[], event:NoteOnEvent, refInd
 	}
 	const endTicks = noteLengthTicks + startTicks;
 	const endPosition = findInsertPosition(track, noteStartIndex, endTicks);
+	// console.log({startPosition,endPosition})
 
 	const checkNoteLength = getTicksBetween(track, startPosition.targetIndex, endPosition.targetIndex) - startPosition.ticksAfterTarget + endPosition.ticksAfterTarget
 	if(checkNoteLength !== noteLengthTicks) {
@@ -30,12 +31,12 @@ export function insertNoteWithLength(track:AnyEvent[], event:NoteOnEvent, refInd
 	const ticksBetweenWrappingItems = getTicksBetween(track, startPosition.targetIndex -2, endPosition.targetIndex + 1);
 	// logDeltaTimesInRange(startPosition.targetIndex-2, endPosition.targetIndex+3, track, [refIndex]);
 
-	insertMidiEvent(track, {...event, subtype:'noteOff'}, endPosition.targetIndex, endPosition.ticksAfterTarget);
+	addMidiEvent(track, {...event, subtype:'noteOff'}, endPosition);
 	
 	// console.log(`inserted end at ${endPosition.targetIndex} and deltaTime of ${endPosition.ticksAfterTarget}`)
 	// logDeltaTimesInRange(startPosition.targetIndex-2, endPosition.targetIndex+3, track, [refIndex]);
 
-	insertMidiEvent(track, {...event, subtype:'noteOn'}, startPosition.targetIndex, startPosition.ticksAfterTarget);
+	addMidiEvent(track, {...event, subtype:'noteOn'}, startPosition);
 
 	// console.log(`inserted start at index ${startPosition.targetIndex} and deltaTime of ${startPosition.ticksAfterTarget}`)
 	// const refIndexUpdated = refIndex > startPosition.targetIndex ? refIndex +1 : refIndex;
@@ -50,12 +51,27 @@ export function insertNoteWithLength(track:AnyEvent[], event:NoteOnEvent, refInd
 }
 
 export function insertMidiEvent(track: AnyEvent[], eventData: ChannelEvent<any>, targetIndex:number, deltaTime:number) {
+	// console.log('insertMidiEvent', {eventData,targetIndex,deltaTime})
+	const position = findInsertPosition(track, targetIndex, deltaTime);
+	addMidiEvent(track, eventData, position);
+}
+
+function addMidiEvent(track: AnyEvent[], eventData: ChannelEvent<any>, pos:InsertPositionInfo) {
+	
 	const event: ChannelEvent<any> = {
 		...eventData,
-		deltaTime: deltaTime
+		deltaTime: pos.ticksAfterTarget
 	};
-	track[targetIndex + 1].deltaTime -= deltaTime;
-	track.splice(targetIndex + 1, 0, event as AnyEvent);
+	if(pos.prepend) {
+		event.deltaTime = 0;
+		track.unshift(event as AnyEvent);
+	} else if(track[pos.targetIndex + 1]) {
+		if(track[pos.targetIndex + 1].deltaTime < pos.ticksAfterTarget) throw new Error('not allowed. Use insertMidiEvent')
+		track[pos.targetIndex + 1].deltaTime -= pos.ticksAfterTarget;
+		track.splice(pos.targetIndex + 1, 0, event as AnyEvent);
+	} else {
+		track.push(event as AnyEvent);
+	}
 }
 
 export function removeMidiEvents(track: AnyEvent[], targetIndex:number, removeCount=1) {
@@ -80,6 +96,7 @@ function logDeltaTimesInRange(startPosition:number, endPosition:number, track: A
 }
 
 interface InsertPositionInfo {
+	prepend: boolean; // special case: the requested position is at or before the beginning of the track.
 	targetIndex: number;
 	advancedTicks: number;
 	ticksAfterTarget: number;
@@ -89,13 +106,20 @@ interface InsertPositionInfo {
 function findInsertPosition(track: AnyEvent[], startIndex: number, ticks: number): InsertPositionInfo {
 	let advancedTicks = 0;
 	let index = startIndex;
-	let targetIndex = -1;
+	let targetIndex = null;
 	const backwards = ticks < 0;
 	let ticksRemaining = Math.abs(ticks);
+	let prepend = false;
 	while (index < track.length && index >= 0) {
 		if(backwards) {
 			const nextTicks = track[index].deltaTime;
-			if (ticksRemaining < nextTicks) {
+			if(index===0) {
+				// this means the note shoul be inserted at the beginning of the track.
+				ticksRemaining -= nextTicks;
+				targetIndex = 0;
+				break;
+			} else if (ticksRemaining < nextTicks) {
+				// console.log('found targetIndex', startIndex,index, ticksRemaining, nextTicks, track[index])
 				targetIndex = index;
 				break;
 			} else {
@@ -104,9 +128,9 @@ function findInsertPosition(track: AnyEvent[], startIndex: number, ticks: number
 			}
 		} else {
 			// deltaTime describes the ticks BEFORE refNote, immediately jump to the next one!
-			const nextTicks = track[index+1].deltaTime;
+			const nextTicks = track[index+1]?.deltaTime;
 			// console.log('nestTicks at '+index+1+' = '+nextTicks)
-			if (ticksRemaining < nextTicks) {
+			if (ticksRemaining < nextTicks && index < track.length) {
 				targetIndex = index;
 				break;
 			} else {
@@ -115,37 +139,47 @@ function findInsertPosition(track: AnyEvent[], startIndex: number, ticks: number
 			}
 		}
 	}
-	if(targetIndex === -1) {
+	if(targetIndex === null) {
 		// no notes in between!
 		targetIndex = startIndex;
 		advancedTicks = 0;
 	} else {
 		advancedTicks = Math.abs(ticks) - ticksRemaining;
 		if(backwards) {
-			// we did a look backwards, but ticksRemaining must be calculated in relation to the index BEFORE.
-			ticksRemaining = track[targetIndex].deltaTime - ticksRemaining;
-			targetIndex--;
+			if(targetIndex > 0) {
+				// we did a look backwards, but ticksRemaining must be calculated in relation to the index BEFORE.
+				ticksRemaining = track[targetIndex].deltaTime - ticksRemaining;
+				targetIndex--;
+			} else {
+				// we are at the front. we cannot do anything else.
+				targetIndex--;
+				prepend = true;
+			}
 		}
 	}
 
-	const between = getTicksBetween(track, startIndex, targetIndex); 
-	let check;
-	if(backwards) {
-		check = between - ticksRemaining
-	} else {
-		check = between + ticksRemaining
-	}
-	// console.log(`findInsertPosition from [${startIndex}] +${ticks} ticks resulted in index ${targetIndex}, ticks inbetween are ${between}, remaining ${ticksRemaining}`)
-	if(check !== Math.abs(ticks)) {
-		throw new Error('This seems off. getTicksBetween calculated tick count of '+check+', which is more than requested tick count '+ticks)
+	// in case a track-prepend is required, the check will fail because the numbers are off.
+	if(!prepend) {
+		const between = getTicksBetween(track, startIndex, targetIndex); 
+		let check;
+		if(backwards) {
+			check = between - ticksRemaining
+		} else {
+			check = between + ticksRemaining
+		}
+		if(check !== Math.abs(ticks)) {
+			console.log({startIndex,ticks,targetIndex, advancedTicks,prepend,between,ticksRemaining,refEvent:track[startIndex]})
+			throw new Error('This seems off. getTicksBetween calculated tick count of '+check+', which is more than requested tick count '+ticks)
+		}
 	}
 
-	return { targetIndex, advancedTicks, ticksAfterTarget:ticksRemaining };
+	return { targetIndex, advancedTicks, ticksAfterTarget:ticksRemaining, prepend };
 }
 
 // ticks between mean we need to sum the deltaTime.
 // INCLUDING deltaTime at target index, IGNORING the deltaTime at start idnex.
 export function getTicksBetween(track:AnyEvent[], start:number, target:number) {
+	if(start===target) return 0;
 	let isNegative = start > target;
 	let ticks = 0;
 	if(isNegative) {
