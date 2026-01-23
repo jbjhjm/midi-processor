d6cfcb168c6b87c41bc3f3cafdf96330b80f91ff
jbjhjm <jannik.wjm@gmail.com>
2026-01-23 15:19:14 +0100

    migration tool: allow to use data from .bak file

--- src/migrate.ts
+++ src/migrate.ts
@@ -4,7 +4,7 @@ import { glob } from 'glob';
 import type { MidiFile } from "midifile-ts";
 import * as path from 'path';
 import color from 'ansi-colors';
-import { collectFiles, sanitizeFilePath } from './utils/files.js';
+import { collectFiles, fileExists, sanitizeFilePath } from './utils/files.js';
 import { globals } from './globals.js';
 
 /**
@@ -17,10 +17,11 @@ const midi = await import("midifile-ts");
 const migrators = await glob('migrators/*.ts', {cwd:path.join(process.cwd(),'src')})
 const migratorSelection = await select({ message:'Select which migration to execute', choices:migrators })
 const importPath = './'+path.normalize(migratorSelection.substring(0, migratorSelection.length - 3)).replaceAll(/\\/g, '/');
-console.log('try to import '+importPath)
 const migratorFn = await import(importPath).then(m=>m.default)
 
-const applyChanges = await input({ message: 'Apply changes? (y/n)' });
+const fromBackup = await input({ message: 'Use backup data? (y/n)', default:'y' });
+const applyChanges = await input({ message: 'Apply changes? (y/n)', default:'y' });
+
 const inputPath = sanitizeFilePath(await input({ message: 'This utility searches for Midi events within a directory. \n'
 											+'Enter custom path or press ENTER', default:globals.path }));
 const globMatcher = await input({ message: 'You can customize the glob matcher to use.', default:globals.glob });
@@ -34,15 +35,26 @@ for (const fileName of await collectFiles(inputPath, globMatcher)) {
 }
 
 async function processFile(relPath:string) {
-		console.log('processFile start '+relPath)
 	const fullPath = path.join(inputPath, relPath);
-	const buffer = await fs.readFile(fullPath)
+	let buffer:Buffer;
+	if(fromBackup==='y') {
+		const backupPath = fullPath + '.bak';
+		if(await fileExists(backupPath)) {
+			buffer = await fs.readFile(backupPath)
+		}
+	}
+	if(!buffer) {
+		buffer = await fs.readFile(fullPath)
+	} 
+
 	const midiData:MidiFile = await midi.read(buffer);
 
 	if(midiData.tracks.length > 1) throw new Error('Multiple Tracks discovered, this is not supported!')
+	const initialLength = midiData.tracks.length;
 	const modified = await migratorFn(midiData, relPath);
-
 	if(!modified) return;
+	const diff = midiData.tracks.length - initialLength;
+	console.log(diff+' MIDI events have been added.')
 
 	if(applyChanges==='y') {
 		const fileBuffer = await midi.write(midiData.tracks, midiData.header.ticksPerBeat);
