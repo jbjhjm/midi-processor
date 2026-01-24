97a8ad2c0889f0ea26f60a0a0529e104ef4e012a
jbjhjm <jannik.wjm@gmail.com>
2026-01-24 12:18:37 +0100

    implement remapper to reduce duplicate code

---
+++ src/utils/remapper.ts
@@ -0,0 +1,102 @@
+import { AnyEvent, NoteOffEvent, NoteOnEvent } from 'midifile-ts';
+import { channelNoteIndex } from './matchers.js';
+import { isMidiNote } from './midi.js';
+
+export type RemappingEntry<TData=any> = [number, number, TData?];
+
+export type RemappingHandlerFn<TRemappingData> = (
+	event: AnyEvent, 
+	mapping: RemappingEntry<TRemappingData>,
+	tools: {
+		applyRegularRemapping:()=>any,
+		addChange:(event:AnyEvent)=>void,
+	}
+) => void;
+
+export interface BatchRemapConfig {
+	fromRange?:[number,number], // start and end
+	fromOffset?:[number,number], // start and count
+	target:{channel:number, start:number}, 
+}
+
+export class Remapper<TRemappingData> {
+	protected mappings = new Map<number, RemappingEntry<TRemappingData>>()
+	protected changeList:AnyEvent[] = []
+	constructor(
+		protected srcChannel:number,
+	) {}
+
+	remap(srcAddress:number, targetChannel:number, targetAddress:number, data={}) {
+		this.mappings.set(
+			channelNoteIndex(this.srcChannel, srcAddress), 
+			[targetChannel,targetAddress,data as TRemappingData]
+		)
+	}
+
+	batchRemap(c:BatchRemapConfig) {
+		let start, count;
+		if(c.fromRange) {
+			start = c.fromRange[0]
+			count = c.fromRange[1] - c.fromRange[0] + 1;
+		} else if(c.fromOffset) {
+			start = c.fromOffset[0]
+			count = c.fromOffset[1];
+		} else {
+			throw new Error('batchRemap failed: provide either fromRange or fromOffset!')
+		}
+		for(let i=0; i<count; i++) {
+			this.remap(start+i, c.target.channel, c.target.start+i)
+		}
+		// console.log('batchRemap entries created', c, this.mappings)
+	}
+
+	apply(track: AnyEvent[], customHandler?:RemappingHandlerFn<TRemappingData>) {
+		// replace events
+		let i = 0;
+		while(i < track.length) {
+			const event = track[i];
+			if(this.isRemappedEvent(event)) {
+				const id = channelNoteIndex(event.channel + 1, event.noteNumber);
+				const applyRegularRemapping = ()=>{
+					const [channel, note, config] = this.mappings.get(id);
+					// console.log('remapping '+event.noteNumber+' to '+note);
+					event.channel = channel - 1;
+					event.noteNumber = note;
+					this.changeList.push(event);
+				}
+				if(customHandler) {
+					customHandler(
+						event, 
+						this.mappings.get(id),
+						{
+							applyRegularRemapping,
+							addChange:(change:AnyEvent)=>{ this.changeList.push(change) }
+						}
+					);
+				} else {
+					applyRegularRemapping()
+				}
+			}
+			i++;
+		}
+
+		return this.changeList;
+	}
+
+	getChangeList() {
+		return this.changeList
+	}
+
+	reset(resetChangeList=false) {
+		this.mappings.clear();
+		if(resetChangeList) this.changeList = [];
+	}
+
+	isRemappedEvent(event:AnyEvent): event is NoteOnEvent|NoteOffEvent {
+		if(!isMidiNote(event)) return false;
+		if(this.srcChannel !== event.channel+1) return false;
+		const id = channelNoteIndex(event.channel+1, event.noteNumber);
+		return this.mappings.has(id)
+	}
+}
+
